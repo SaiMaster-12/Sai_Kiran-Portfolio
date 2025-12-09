94669c93583e6fd0c9bbdc712b67180458b33df4
Sai Master <saimaster@Sais-MacBook-Pro.local>
2025-12-09 12:19:04 +0530

    implement dock presentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Navbar from '#components/navbar.jsx'
 import Welcome from '#components/Welcome'
+import Dock from '#components/Dock'
 
-export {Navbar, Welcome};
+
+export {Navbar, Welcome, Dock};
