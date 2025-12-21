d35afc99b7dbed03998b452d6e8065302114ce58
Sai Kiran J <saikiranjavalkar@gmail.com>
2025-12-21 23:23:47 +0530

    Fix import case

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Navbar from '#components/navbar.jsx'
+import Navbar from '#components/Navbar.jsx'
 import Welcome from '#components/Welcome'
 import Dock from '#components/Dock'
 import WindowControlls from './WindowControls';
